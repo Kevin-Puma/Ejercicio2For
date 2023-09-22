fe67ed2b04e5711b73e65ad7077fafc40c827673
User <User@DESKTOP-CRJD420>
2023-09-21 23:19:26 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2For/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ingrese la base: ");
+int numeroBase = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el exponente: ");
+int exponente = int.Parse(Console.ReadLine());
+
+int resultado = 1;
+
+for (int i = 0; i < exponente; i++)
+{
+resultado = resultado * numeroBase;
+}
+
+Console.WriteLine("La potencia es: " +resultado+ "");
